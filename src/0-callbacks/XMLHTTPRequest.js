510c4e5da52919ff0311f2ebd742a7310b3dc718
Luis Mendoza <lmendoza.mg@gmail.com>
2023-07-25 08:32:42 -0400

    feat: using XMLHttpRequest

---
+++ src/0-callbacks/XMLHTTPRequest.js
@@ -0,0 +1,24 @@
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+const API = 'https://rickandmortyapi.com/api'
+
+const fetchData = (urlAPI, callback) => {
+  const xhttp = new XMLHttpRequest()
+
+  xhttp.addEventListener("load", () => {
+    callback(null, JSON.parse(xhttp.responseText)) 
+  })
+
+  xhttp.addEventListener("error", () => {
+    const error = new Error('Error: ' + urlAPI)
+    callback(error, null)
+  })
+
+  xhttp.open('GET', urlAPI)
+  xhttp.send()
+}
+
+fetchData(`${API}/character/1`, (error, data) => {
+  if (error) return console.log(error)
+
+  console.log(data)
+})
