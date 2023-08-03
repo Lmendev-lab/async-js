ff65fcf320bb584c92ea604e3295f86b47a60be0
Luis Mendoza <lmendoza.mg@gmail.com>
2023-08-03 07:55:10 -0400

    feat: promises example

--- src/1-promises/promises.js
+++ src/1-promises/promises.js
@@ -2,10 +2,25 @@
 const delay = (time, message) =>
   new Promise((resolve, reject) => {
     if (time < 0) reject("Time cannot be negative")
-    
+
     window.setTimeout(() => {
       resolve(message)
     }, time)
   })
 
-delay(2000, "Hello after 2s").then(message => console.log(message))
+delay(2000, "Hello after 2s")
+  .then(message => console.log(message))
+
+// Cows example
+
+let cows = 10
+
+const countCows = new Promise((resolve, reject) => {
+  if(cows < 10) reject("Cows: 🚫")
+
+  resolve("Cows: OK 🐄")
+})
+
+countCows
+  .then(msg => console.log(`then: ${msg}`))
+  .catch(msg => console.log(`catch: ${msg}`))
