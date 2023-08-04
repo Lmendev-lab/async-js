cc46287f9444ecd084f92d74db4a6f683b68be7e
Luis Mendoza <lmendoza.mg@gmail.com>
2023-08-04 08:35:54 -0400

    feat: use async await

---
+++ src/2-async-await/async-await.mjs
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+const API = 'https://rickandmortyapi.com/api'
+
+const fetchData = async urlAPI => {
+  const response = await fetch(urlAPI)
+  const data = response.json()
+
+  return data
+}
+
+const getCharacterInfo = async () => {
+  const data = await fetchData(`${API}/character/1`)
+
+  console.log(`Ya se encuentran cargados los datos de: ${data.name}`)
+}
+
+getCharacterInfo()
+console.log('Bienvenido al programa')
